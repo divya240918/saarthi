8f2bf7ded9e4ca2daddb23d374cc5c90cefe3ebc
Divya <dasdivya589@gmail.com>
2026-06-07 19:06:39 +0530

    Completed basic set-up

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./connect/db.js"
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => {
+    res.json({ message: "Server is running"});
+} )
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running at PORT ${process.env.PORT}`);
+});
